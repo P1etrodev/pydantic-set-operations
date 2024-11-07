09adeca877b23ba223f53767f2b0101652ca4d92
p1etrodev <francodavidpietrokovsky@gmail.com>
2024-11-07 12:58:16 -0300

    updated README.md

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
 	name="pydantic_set_operations",
-	version="1.0.7",
+	version="1.0.8",
 	description="An enhanced version of Pydantic's BaseModel, allowing for advanced model "
 	            "manipulations.",
 	package_dir={"": "src"},
