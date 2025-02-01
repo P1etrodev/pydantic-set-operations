8e14f1cc5bcfbd2a5460cd0133d0f418a3bf8fa8
Franco Pietrokovsky <Francodavidpietrokovsky@gmail.com>
2025-02-01 16:51:54 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
 	name="pydantic_set_operations",
-	version="1.0.17",
+	version="1.0.18",
 	description="An enhanced version of Pydantic's BaseModel, allowing for advanced model "
 	            "manipulations. (Inspired in TypeScript)",
 	package_dir={"": "src"},
